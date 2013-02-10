125a8e7cf6b05d3762ab6ecd6135846612cd9ccc
Dave Rusek <dave.rusek@gmail.com>
2013-02-10 10:49:10 -0700

    Added correct equals/hashcode/toString implementation to RtpParticipant

--- src/main/java/com/biasedbit/efflux/participant/RtpParticipant.java
+++ src/main/java/com/biasedbit/efflux/participant/RtpParticipant.java
@@ -19,6 +19,7 @@ package com.biasedbit.efflux.participant;
 import com.biasedbit.efflux.packet.DataPacket;
 import com.biasedbit.efflux.packet.SdesChunk;
 import com.biasedbit.efflux.util.TimeUtils;
+import com.google.common.base.Objects;
 
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -31,241 +32,252 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class RtpParticipant {
 
-    // constants ------------------------------------------------------------------------------------------------------
+  // constants ------------------------------------------------------------------------------------------------------
 
-    private static final int VALID_PACKETS_UNTIL_VALID_PARTICIPANT = 3;
+  private static final int VALID_PACKETS_UNTIL_VALID_PARTICIPANT = 3;
 
-    // configuration --------------------------------------------------------------------------------------------------
+  // configuration --------------------------------------------------------------------------------------------------
 
-    private final RtpParticipantInfo info;
+  private final RtpParticipantInfo info;
 
-    // internal vars --------------------------------------------------------------------------------------------------
+  // internal vars --------------------------------------------------------------------------------------------------
 
-    private SocketAddress dataDestination;
-    private SocketAddress controlDestination;
-    private SocketAddress lastDataOrigin;
-    private SocketAddress lastControlOrigin;
-    private long lastReceptionInstant;
-    private long byeReceptionInstant;
-    private int lastSequenceNumber;
-    private boolean receivedSdes;
-    private final AtomicLong receivedByteCounter;
-    private final AtomicLong receivedPacketCounter;
-    private final AtomicInteger validPacketCounter;
+  private SocketAddress dataDestination;
+  private SocketAddress controlDestination;
+  private SocketAddress lastDataOrigin;
+  private SocketAddress lastControlOrigin;
+  private long lastReceptionInstant;
+  private long byeReceptionInstant;
+  private int lastSequenceNumber;
+  private boolean receivedSdes;
+  private final AtomicLong receivedByteCounter;
+  private final AtomicLong receivedPacketCounter;
+  private final AtomicInteger validPacketCounter;
 
-    // constructors ---------------------------------------------------------------------------------------------------
+  // constructors ---------------------------------------------------------------------------------------------------
 
-    private RtpParticipant(RtpParticipantInfo info) {
-        // For internal use only.
-        this.info = info;
+  private RtpParticipant(RtpParticipantInfo info) {
+    // For internal use only.
+    this.info = info;
 
-        this.lastSequenceNumber = -1;
-        this.lastReceptionInstant = 0;
-        this.byeReceptionInstant = 0;
+    this.lastSequenceNumber = -1;
+    this.lastReceptionInstant = 0;
+    this.byeReceptionInstant = 0;
 
-        this.receivedByteCounter = new AtomicLong();
-        this.receivedPacketCounter = new AtomicLong();
-        this.validPacketCounter = new AtomicInteger();
+    this.receivedByteCounter = new AtomicLong();
+    this.receivedPacketCounter = new AtomicLong();
+    this.validPacketCounter = new AtomicInteger();
+  }
+
+  // public static methods ------------------------------------------------------------------------------------------
+
+  public static RtpParticipant createReceiver(String host, int dataPort, int controlPort) {
+    RtpParticipant participant = new RtpParticipant(new RtpParticipantInfo());
+
+    if ((dataPort < 0) || (dataPort > 65536)) {
+      throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
+    }
+    if ((controlPort < 0) || (controlPort > 65536)) {
+      throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
     }
 
-    // public static methods ------------------------------------------------------------------------------------------
+    participant.dataDestination = new InetSocketAddress(host, dataPort);
+    participant.controlDestination = new InetSocketAddress(host, controlPort);
 
-    public static RtpParticipant createReceiver(String host, int dataPort, int controlPort) {
-        RtpParticipant participant = new RtpParticipant(new RtpParticipantInfo());
+    return participant;
+  }
 
-        if ((dataPort < 0) || (dataPort > 65536)) {
-            throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
-        }
-        if ((controlPort < 0) || (controlPort > 65536)) {
-            throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
-        }
+  public static RtpParticipant createReceiver(RtpParticipantInfo info, String host, int dataPort, int controlPort) {
+    RtpParticipant participant = new RtpParticipant(info);
 
-        participant.dataDestination = new InetSocketAddress(host, dataPort);
-        participant.controlDestination = new InetSocketAddress(host, controlPort);
-
-        return participant;
+    if ((dataPort < 0) || (dataPort > 65536)) {
+      throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
+    }
+    if ((controlPort < 0) || (controlPort > 65536)) {
+      throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
     }
 
-    public static RtpParticipant createReceiver(RtpParticipantInfo info, String host, int dataPort, int controlPort) {
-        RtpParticipant participant = new RtpParticipant(info);
+    participant.dataDestination = new InetSocketAddress(host, dataPort);
+    participant.controlDestination = new InetSocketAddress(host, controlPort);
 
-        if ((dataPort < 0) || (dataPort > 65536)) {
-            throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
-        }
-        if ((controlPort < 0) || (controlPort > 65536)) {
-            throw new IllegalArgumentException("Invalid port number; use range [0;65536]");
-        }
+    return participant;
+  }
 
-        participant.dataDestination = new InetSocketAddress(host, dataPort);
-        participant.controlDestination = new InetSocketAddress(host, controlPort);
-
-        return participant;
-    }
-
-    public static RtpParticipant createFromUnexpectedDataPacket(SocketAddress origin, DataPacket packet) {
-        RtpParticipant participant = new RtpParticipant(new RtpParticipantInfo(packet.getSsrc()));
-        participant.lastDataOrigin = origin;
+  public static RtpParticipant createFromUnexpectedDataPacket(SocketAddress origin, DataPacket packet) {
+    RtpParticipant participant = new RtpParticipant(new RtpParticipantInfo(packet.getSsrc()));
+    participant.lastDataOrigin = origin;
 //        participant.lastSequenceNumber = packet.getSequenceNumber();
 //        participant.lastReceptionInstant = TimeUtils.now();
 //        participant.receivedByteCounter.addAndGet(packet.getDataSize());
 //        participant.receivedPacketCounter.incrementAndGet();
 
-        return participant;
+    return participant;
+  }
+
+  public static RtpParticipant createFromSdesChunk(SocketAddress origin, SdesChunk chunk) {
+    RtpParticipant participant = new RtpParticipant(new RtpParticipantInfo());
+    participant.lastControlOrigin = origin;
+    participant.getInfo().updateFromSdesChunk(chunk);
+    participant.receivedSdes();
+
+    return participant;
+  }
+
+  // public methods -------------------------------------------------------------------------------------------------
+
+  public long resolveSsrcConflict(long ssrcToAvoid) {
+    // Will hardly ever loop more than once...
+    while (this.getSsrc() == ssrcToAvoid) {
+      this.getInfo().setSsrc(RtpParticipantInfo.generateNewSsrc());
     }
 
-    public static RtpParticipant createFromSdesChunk(SocketAddress origin, SdesChunk chunk) {
-        RtpParticipant participant = new RtpParticipant(new RtpParticipantInfo());
-        participant.lastControlOrigin = origin;
-        participant.getInfo().updateFromSdesChunk(chunk);
-        participant.receivedSdes();
+    return this.getSsrc();
+  }
 
-        return participant;
+  public long resolveSsrcConflict(Collection<Long> ssrcsToAvoid) {
+    // Probability to execute more than once is higher than the other method that takes just a long as parameter,
+    // but its still incredibly low: for 1000 participants, there's roughly 2*10^-7 chance of collision
+    while (ssrcsToAvoid.contains(this.getSsrc())) {
+      this.getInfo().setSsrc(RtpParticipantInfo.generateNewSsrc());
     }
 
-    // public methods -------------------------------------------------------------------------------------------------
+    return this.getSsrc();
+  }
 
-    public long resolveSsrcConflict(long ssrcToAvoid) {
-        // Will hardly ever loop more than once...
-        while (this.getSsrc() == ssrcToAvoid) {
-            this.getInfo().setSsrc(RtpParticipantInfo.generateNewSsrc());
-        }
 
-        return this.getSsrc();
+  public void byeReceived() {
+    this.byeReceptionInstant = TimeUtils.now();
+  }
+
+  public void receivedSdes() {
+    this.receivedSdes = true;
+  }
+
+  public void packetReceived() {
+    this.lastReceptionInstant = TimeUtils.now();
+  }
+
+  public boolean isReceiver() {
+    return (this.dataDestination != null) && (this.controlDestination != null);
+  }
+
+  // getters & setters ----------------------------------------------------------------------------------------------
+
+  public long getSsrc() {
+    return this.getInfo().getSsrc();
+  }
+
+  public RtpParticipantInfo getInfo() {
+    return info;
+  }
+
+  public long getLastReceptionInstant() {
+    return lastReceptionInstant;
+  }
+
+  public long getByeReceptionInstant() {
+    return byeReceptionInstant;
+  }
+
+  public int getLastSequenceNumber() {
+    return lastSequenceNumber;
+  }
+
+  public void setLastSequenceNumber(int lastSequenceNumber) {
+    this.lastSequenceNumber = lastSequenceNumber;
+  }
+
+  public boolean receivedBye() {
+    return this.byeReceptionInstant > 0;
+  }
+
+  public long getReceivedPackets() {
+    return this.receivedPacketCounter.get();
+  }
+
+  public long getReceivedBytes() {
+    return this.receivedByteCounter.get();
+  }
+
+  public boolean hasReceivedSdes() {
+    return receivedSdes;
+  }
+
+  public SocketAddress getDataDestination() {
+    return dataDestination;
+  }
+
+  public void setDataDestination(SocketAddress dataDestination) {
+    if (dataDestination == null) {
+      throw new IllegalArgumentException("Argument cannot be null");
+    }
+    this.dataDestination = dataDestination;
+  }
+
+  public SocketAddress getControlDestination() {
+    return controlDestination;
+  }
+
+  public void setControlDestination(SocketAddress controlDestination) {
+    if (dataDestination == null) {
+      throw new IllegalArgumentException("Argument cannot be null");
+    }
+    this.controlDestination = controlDestination;
+  }
+
+  public SocketAddress getLastDataOrigin() {
+    return lastDataOrigin;
+  }
+
+  public void setLastDataOrigin(SocketAddress lastDataOrigin) {
+    this.lastDataOrigin = lastDataOrigin;
+  }
+
+  public SocketAddress getLastControlOrigin() {
+    return lastControlOrigin;
+  }
+
+  public void setLastControlOrigin(SocketAddress lastControlOrigin) {
+    this.lastControlOrigin = lastControlOrigin;
+  }
+
+  // low level overrides --------------------------------------------------------------------------------------------
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (this == o) {
+      return true;
     }
 
-    public long resolveSsrcConflict(Collection<Long> ssrcsToAvoid) {
-        // Probability to execute more than once is higher than the other method that takes just a long as parameter,
-        // but its still incredibly low: for 1000 participants, there's roughly 2*10^-7 chance of collision
-        while (ssrcsToAvoid.contains(this.getSsrc())) {
-            this.getInfo().setSsrc(RtpParticipantInfo.generateNewSsrc());
-        }
-
-        return this.getSsrc();
+    if (!(o instanceof RtpParticipant)) {
+      return false;
     }
 
+    final RtpParticipant that = (RtpParticipant) o;
+    return Objects.equal(this.controlDestination, that.controlDestination) &&
+      Objects.equal(this.dataDestination, that.dataDestination);
+  }
 
-    public void byeReceived() {
-        this.byeReceptionInstant = TimeUtils.now();
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(dataDestination, controlDestination);
+  }
 
-    public void receivedSdes() {
-        this.receivedSdes = true;
-    }
-
-    public void packetReceived() {
-        this.lastReceptionInstant = TimeUtils.now();
-    }
-
-    public boolean isReceiver() {
-        return (this.dataDestination != null) && (this.controlDestination != null);
-    }
-
-    // getters & setters ----------------------------------------------------------------------------------------------
-
-    public long getSsrc() {
-        return this.getInfo().getSsrc();
-    }
-
-    public RtpParticipantInfo getInfo() {
-        return info;
-    }
-
-    public long getLastReceptionInstant() {
-        return lastReceptionInstant;
-    }
-
-    public long getByeReceptionInstant() {
-        return byeReceptionInstant;
-    }
-
-    public int getLastSequenceNumber() {
-        return lastSequenceNumber;
-    }
-
-    public void setLastSequenceNumber(int lastSequenceNumber) {
-        this.lastSequenceNumber = lastSequenceNumber;
-    }
-
-    public boolean receivedBye() {
-        return this.byeReceptionInstant > 0;
-    }
-
-    public long getReceivedPackets() {
-        return this.receivedPacketCounter.get();
-    }
-
-    public long getReceivedBytes() {
-        return this.receivedByteCounter.get();
-    }
-
-    public boolean hasReceivedSdes() {
-        return receivedSdes;
-    }
-
-    public SocketAddress getDataDestination() {
-        return dataDestination;
-    }
-
-    public void setDataDestination(SocketAddress dataDestination) {
-        if (dataDestination == null) {
-            throw new IllegalArgumentException("Argument cannot be null");
-        }
-        this.dataDestination = dataDestination;
-    }
-
-    public SocketAddress getControlDestination() {
-        return controlDestination;
-    }
-
-    public void setControlDestination(SocketAddress controlDestination) {
-        if (dataDestination == null) {
-            throw new IllegalArgumentException("Argument cannot be null");
-        }
-        this.controlDestination = controlDestination;
-    }
-
-    public SocketAddress getLastDataOrigin() {
-        return lastDataOrigin;
-    }
-
-    public void setLastDataOrigin(SocketAddress lastDataOrigin) {
-        this.lastDataOrigin = lastDataOrigin;
-    }
-
-    public SocketAddress getLastControlOrigin() {
-        return lastControlOrigin;
-    }
-
-    public void setLastControlOrigin(SocketAddress lastControlOrigin) {
-        this.lastControlOrigin = lastControlOrigin;
-    }
-
-    // low level overrides --------------------------------------------------------------------------------------------
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof RtpParticipant)) {
-            return false;
-        }
-
-        RtpParticipant that = (RtpParticipant) o;
-        return this.controlDestination.equals(that.controlDestination) &&
-               this.dataDestination.equals(that.dataDestination) &&
-               this.info.getCname().equals(that.info.getCname());
-    }
-
-    @Override
-    public int hashCode() {
-        int result = dataDestination.hashCode();
-        result = 31 * result + controlDestination.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return this.getInfo().toString();
-    }
+  @Override
+  public String toString() {
+    return Objects.toStringHelper(this)
+      .add("datqDestination", dataDestination)
+      .add("controlDestination", controlDestination)
+      .add("lastDataOrigin", lastDataOrigin)
+      .add("lastControlOrigin", lastControlOrigin)
+      .add("lastReceptionInstant", lastReceptionInstant)
+      .add("byeReceptionInstant", byeReceptionInstant)
+      .add("lastSequenceNumber", lastSequenceNumber)
+      .add("receivedSdes", receivedSdes)
+      .add("receivedByteCounter", receivedByteCounter)
+      .add("receivedPacketCounter", receivedPacketCounter)
+      .add("validPacketCounter", validPacketCounter)
+      .toString();
+  }
 }
